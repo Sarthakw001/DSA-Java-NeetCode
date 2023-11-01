4a6661e7900414a36153a030d616bec0f68f8f52
Sarthak Bhatnagar <sarthakb@Sarthaks-MacBook-Air.local>
2023-11-01 20:20:39 +0530

    day_4

--- Arrays & Hashing/LongestCommonPrefix.java
+++ Arrays & Hashing/LongestCommonPrefix.java
@@ -3,16 +3,41 @@
  * If there is no common prefix, return an empty string "".
  */
 
+import java.util.Arrays;
+
 public class LongestCommonPrefix {
-    public String commonPrefix(String s,String t){
-        String ans = "";
+    public String longestCommonPrefix1(String[] strs) {
+        String ans = strs[0];
+        String str = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int m = 0, n = 0;
+            String tempAns = "";
+            while (m < str.length() && n < strs[i].length()) {
+                if (str.charAt(m) == strs[i].charAt(n))
+                    tempAns += str.charAt(m);
+                else
+                    break;
+                m++;
+                n++;
+            }
+            if (tempAns.length() < ans.length())
+                ans = tempAns;
+        }
         return ans;
     }
-    public String longestCommonPrefix(String[] strs) {
-        String ans = "";
-        for(int i=0;i<strs.length;i++){
-            int j = i+1;
 
+    public String longestCommonPrefix2(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length - 1];
+        String ans = "";
+        int m = 0, n = 0;
+        while (m < s1.length() && n < s2.length()) {
+            if (s1.charAt(m) != s2.charAt(n))
+                break;
+            ans += s1.charAt(m);
+            m++;
+            n++;
         }
         return ans;
     }
